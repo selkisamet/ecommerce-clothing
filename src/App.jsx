edebbe527cfc33e1270f2f12995febb5165df39d
Samet <sametselki@outlook.com>
2023-02-27 01:38:26 +0300

    twelfth commit | lazy and suspense were used for performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import Home from "./routes/home/home.components";
 import { Routes, Route } from "react-router-dom";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
+import { lazy, Suspense } from "react";
+import Loading from "./components/loading.component";
+
+const Home = lazy(() => import("./routes/home/home.components"));
+const Navigation = lazy(() => import("./routes/navigation/navigation.component"));
+const Authentication = lazy(() => import("./routes/authentication/authentication.component"));
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
-
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
---
+++ src/components/loading.component.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div style={{ color: "white", backgroundColor: "red" }}>
+            <p>Loading...</p>
+        </div>
+    )
+}
+
+export default Loading;
